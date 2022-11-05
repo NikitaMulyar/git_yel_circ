195a37c37bf1779fcba19ad0af72e42b69e06f6c
NikitaMulyar <nickm.school@mail.ru>
2022-11-05 13:48:46 +0300

    Second task Y.Lyceum

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print('My first git-repo')
+import sys
+from random import randint
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
+from PyQt6 import uic
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('yel_circ.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.run(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def run(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        a = randint(50, 400)
+        b = randint(50, 400)
+        qp.drawEllipse(100, 100, a, a)
+        qp.drawEllipse(550, 100, b, b)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
